ae3b5e18bb6f194a0ed084c3296e7121e169e179
Anthony Ngo <Anthony.v.ngo@gmail.com>
2023-05-07 22:56:26 -0700

    add styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,11 +10,52 @@ const Button = () => {
 
   return (
   <>
-  <button onClick={handleClick} className='rounded-full w-24 h-12 relative z-50'>
-    <div className={`${isClicked ? 'translate-x-full' : 'translate-x-0'} w-12 h-12 rounded-full border-white border`}>
+  {/* <button onClick={handleClick} className='rounded-full w-24 h-12 relative z-50 border border-green-300'>
+    <div className={`${isClicked ? 'translate-x-full' : 'translate-x-0'} w-12 h-12 rounded-full border-orange-400 border`}/>
+    <div className={`${isClicked ? 'top-0 bg-slate-500' : 'bottom-0 bg-blue-300'} h-36 absolute w-24 -z-10 transition-all`}>
     </div>
-    <div className={`${isClicked ? 'top-0 bg-slate-500' : 'bottom-0 bg-blue-300'} h-36  absolute w-24 z-10`}>
+  </button> */}
+  {/* <div className='relative border border-black w-24 h-24'>
+    <div className='absolute z-20 h-12 w-12 bg-transparent border-[2px] rounded-full border-white'>
+
     </div>
+    <div className='bg-blue-300 h-24 w-24 absolute z-10'></div>
+  </div> */}
+  {/* <div class="hasCircle">
+    <div className='h-[175px] w-[150px] bg-blue-300 absolute top-0'>
+
+    </div>
+  </div> */}
+  <button onClick={handleClick} class="hasCircle w-[150px] h-[150px] border flex items-center relative overflow-hidden">
+    <div className={`${isClicked ? 'bg-slate-500 translate-y-[61px]' : ' bg-blue-300  translate-y-[-61px]'} h-[175px] w-[150px] absolute transition-all duration-1000`}>
+      <div className='flex flex-col h-full justify-between'>
+        <div className='w-full'>
+          <div className='flex text-white'>
+            <span>stars</span>
+            <span>stars</span>
+            <span>stars</span>
+          </div>
+          <div className='flex text-white'>  
+            <span>stars</span>
+            <span>stars</span>
+            <span>stars</span>
+          </div>
+        </div>
+        <div className='w-full'>
+          <div className='flex text-white'>
+            <span>clouds</span>
+            <span>clouds</span>
+            <span>clouds</span>
+          </div>
+          <div className='flex text-white'>
+            <span>clouds</span>
+            <span>clouds</span>
+            <span>clouds</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className={`${isClicked ? 'translate-x-[101px] bg-slate-200' : 'translate-x-[1px] bg-yellow-300 '} w-12 h-12 rounded-full transition-all duration-1000`}/>
   </button>
   </>
   )
