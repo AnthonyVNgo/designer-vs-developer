2941b8befc39de5a2d78354ed4fc27545dd9a23d
Anthony Ngo <Anthony.v.ngo@gmail.com>
2023-05-08 00:27:08 -0700

    add more styles to toggle

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -26,19 +26,22 @@ const Button = () => {
 
     </div>
   </div> */}
-  <button onClick={handleClick} class="hasCircle w-[150px] h-[150px] border flex items-center relative overflow-hidden">
+  {/* <button onClick={handleClick} class="hasCircle w-[150px] h-[150px] border flex items-center relative overflow-hidden"> */}
+  <button onClick={handleClick} class="hasCircle w-[150px] h-[150px] flex items-center relative overflow-hidden">
     <div className={`${isClicked ? 'bg-slate-500 translate-y-[61px]' : ' bg-blue-300  translate-y-[-61px]'} h-[175px] w-[150px] absolute transition-all duration-1000`}>
       <div className='flex flex-col h-full justify-between'>
         <div className='w-full'>
-          <div className='flex text-white'>
-            <span>stars</span>
-            <span>stars</span>
-            <span>stars</span>
+          <div className='flex items-center justify-around text-white'>
+            <span className='text-[7px]'>&#10022;</span>
+            <span className='text-[14px]'>&#10022;</span>
+            <span className='text-[5px]'>&#10022;</span>
+            <span className='text-[9px]'>&#10022;</span>
           </div>
-          <div className='flex text-white'>  
-            <span>stars</span>
-            <span>stars</span>
-            <span>stars</span>
+          <div className='flex items-center justify-around text-white'>  
+            <span className='text-[13px]'>&#10022;</span>
+            <span className='text-[7px]'>&#10022;</span>
+            <span className='text-[5px]'>&#10022;</span>
+            <span className='text-[16px]'>&#10022;</span>
           </div>
         </div>
         <div className='w-full'>
@@ -55,7 +58,9 @@ const Button = () => {
         </div>
       </div>
     </div>
-    <div className={`${isClicked ? 'translate-x-[101px] bg-slate-200' : 'translate-x-[1px] bg-yellow-300 '} w-12 h-12 rounded-full transition-all duration-1000`}/>
+    <div className={`${isClicked ? 'translate-x-[101px] bg-slate-200' : 'translate-x-[1px] bg-yellow-300 '} w-12 h-12 rounded-full transition-all duration-1000 outline outline-[30px] outline-slate-100/50 `}>
+      <div className={`w-12 h-12 rounded-full transition-all duration-1000 outline outline-[15px] outline-slate-100/25 `}/>
+    </div>
   </button>
   </>
   )
